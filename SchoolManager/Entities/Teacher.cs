03d5c27ffc90af95aa32eabbd05f42c23b14898e
Vitor Pimentel <vitorpimentel493@gmail.com>
2025-08-03 00:25:14 -0300

    Adição das classes Teacher e TeacherManager

---
+++ SchoolManager/Entities/Teacher.cs
@@ -0,0 +1,22 @@
+﻿
+namespace SchoolManager.Entities
+{
+    internal class Teacher : Person
+    {
+        public double MonthlySalary { get; private set; }
+        public int ClassInWeek { get; private set; }
+        public Teacher() 
+        {
+            
+        }
+        public Teacher(int id, string name, double monthlySalary, int classInWeek) : base(id, name)
+        {
+            MonthlySalary = monthlySalary;
+            ClassInWeek = classInWeek;
+        }
+        public override string ToString()
+        {
+            return $"{Id},{Name}, salário: {MonthlySalary}";
+        }
+    }
+}
---
+++ SchoolManager/Services/TeacherManager.cs
@@ -0,0 +1,37 @@
+﻿using SchoolManager.Entities;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace SchoolManager.Services
+{
+    internal class TeacherManager : IManager<Teacher>
+    {
+        Dictionary<int, Teacher> teachers = new Dictionary<int, Teacher>();
+        public TeacherManager()
+        {
+        }
+        public bool TryAdd(Teacher teacher)
+        {
+            return teachers.TryAdd(teacher.Id, teacher);
+        }
+        public bool Remove(int id)
+        {
+            return teachers.Remove(id);
+        }
+        public Dictionary<int, Teacher> GetAll()
+        {
+            return teachers;
+        }
+        public void PrintAll()
+        {
+            foreach(var teacher in teachers.Values)
+            {
+                Console.WriteLine(teacher);
+            }
+        }
+        public bool IsEmpty()
+        {
+            return teachers.Count == 0;
+        }
+    }
+}
