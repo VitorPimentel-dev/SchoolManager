77b8e92cadcc81e7e92c43ad77b4674cb1bdf6ec
Vitor Pimentel <vitorpimentel493@gmail.com>
2025-08-03 22:51:47 -0300

    Adição da classe Subject

---
+++ SchoolManager/Entities/Subject.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace SchoolManager.Entities
+{
+    internal class Subject
+    {
+        public string Name { get; set; }
+        public int Code { get; set; }
+        public int credits { get; set; }
+
+        public Teacher AssignedTeacher { get; set; }
+
+        public Subject() 
+        {
+        
+        }
+        public Subject(string name, int code, int credits, Teacher assignedTeacher)
+        {
+            Name = name;
+            Code = code;
+            this.credits = credits;
+            AssignedTeacher = assignedTeacher;
+        }
+    }
+}
